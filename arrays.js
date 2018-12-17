14d948d14f9ba5de6cb62acf218315e46d519b34
Verity Hill <verity.lowenna@gmail.com>
2018-12-17 19:28:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  [...array, "element"];
+  [...array, `${element}`];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
